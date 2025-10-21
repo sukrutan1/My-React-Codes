28d48427e8e81264d11e111da987511decfed1a4
sukrutan1 <sukrutan@posta.mu.edu.tr>
2025-10-21 21:36:32 +0300

    21-October

---
+++ 08 React Components/src/UList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MYfoods() {
+  return (
+    <ul>
+      <li>Mantı</li>
+      <li>Pizza</li>
+      <li>Mixed fry</li>
+    </ul>
+  );
+}
+
+export default MYfoods;
---
+++ 08 React Components/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import MYfoods from "./UList";
+
+ReactDOM.render(
+  <div>
+    <Heading></Heading>
+    <MYfoods></MYfoods>
+  </div>,
+  document.getElementById("root")
+);
