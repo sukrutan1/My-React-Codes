59618a2354f581a7b5bfd2df54a9832a364a1b75
sukrutan1 <sukrutan@posta.mu.edu.tr>
2025-10-26 22:03:07 +0300

    New Update

---
+++ 12 Keeper App Project Part-1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import NoteBox from "./Note";
+import Footer from "./Footer";
+
+function app() {
+  return (
+    <div>
+      <Header />
+      <NoteBox />
+      <Footer />
+    </div>
+  );
+}
+
+export default app;
